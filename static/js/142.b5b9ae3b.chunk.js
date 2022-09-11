a782d85517692f2a0a6247da3e8b86b463e1f0d3
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-09-11 08:17:23 +0000

    Deploying to gh-pages from @ Kaplynenko/goit-react-hw-08-phonebook@28bdd5bdcf62c5abfea7e7e1c692f553cc6cc977 🚀

---
+++ static/js/142.b5b9ae3b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[142],{7142:function(e,n,o){o.r(n),o.d(n,{default:function(){return u}});var t=o(184);function u(){return(0,t.jsx)("h1",{children:"404 Not found page :("})}}}]);
+//# sourceMappingURL=142.b5b9ae3b.chunk.js.map
---
+++ static/js/898.2f53303f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[898],{2898:function(e,n,o){o.r(n),o.d(n,{default:function(){return s}});var r=o(184);function s(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsxs)("section",{children:[(0,r.jsx)("h1",{children:"Phone book"}),(0,r.jsx)("p",{children:"Welcome to the Phone book."}),(0,r.jsx)("p",{children:"Please Register."}),(0,r.jsx)("p",{children:"If you are a registered user please Sign in."})]})})}}}]);
+//# sourceMappingURL=898.2f53303f.chunk.js.map
