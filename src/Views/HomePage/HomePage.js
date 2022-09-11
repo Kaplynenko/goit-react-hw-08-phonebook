28bdd5bdcf62c5abfea7e7e1c692f553cc6cc977
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-09-11 11:15:26 +0300

    fix

---
+++ src/Views/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <>
+      <section>
+        <h1>Phone book</h1>
+        <p>Welcome to the Phone book.</p>
+        <p>Please Register.</p>
+        <p>If you are a registered user please Sign in.</p>
+      </section>
+    </>
+  );
+}
